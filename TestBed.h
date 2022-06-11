36028469859ea1ae9a092d8dd632ec1363aced34
Yileri <75039627+Yileri@users.noreply.github.com>
2022-06-11 13:18:03 +0300

    files are added

---
+++ TestBed.h
@@ -0,0 +1,21 @@
+// write your codes here
+#ifndef TESTBED_H
+#define TESTBED_H
+
+#include "SelectionAlgorithm.h"
+#include "AlgorithmSortAll.h"
+#include "AlgorithmSortK.h"
+#include "AlgorithmSortHeap.h"
+
+class TestBed {
+
+	SelectionAlgorithm* algorithm;
+
+public:
+	TestBed();
+	~TestBed();
+	void execute();
+	void setAlgorithm(int, int);
+};
+
+#endif
